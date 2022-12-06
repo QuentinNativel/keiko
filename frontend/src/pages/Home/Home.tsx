414a4e0a0c97d038c52750e92354908dc9aa69d5
Quentin Nativel <nativelq@Quentins-MacBook-Pro.local>
2022-12-06 10:44:02 +0100

    Create pokemon Carapuce in Home page

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -5,6 +5,17 @@ export const Home = () => {
     <div className={styles.intro}>
       <div>Bienvenue sur ton futur pokédex !</div>
       <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
+      <p>Test 1..2</p>
+      <div>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" alt="" />
+        <p>
+          Name: <span>Carapuce</span>
+        </p>
+
+        <p>
+          Number: <span>7</span>
+        </p>
+      </div>
     </div>
   )
 }
