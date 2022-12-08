87ef397d90284d95dcdf62cd1b9045b1ecf7b8dd
Quentin Nativel <quentinn@theodo.fr>
2022-12-08 10:44:16 +0100

    Add HOC, breaks css :/

--- frontend/src/components/Animate/Animate.tsx
+++ frontend/src/components/Animate/Animate.tsx
@@ -1,1 +1,18 @@
-export const Animate = () => <></>
+import { useState } from "react"
+import styles from "./Animate.module.css"
+
+export const Animate = <P extends object>(BaseComponent: React.ComponentType<P>) => {
+  const Animate = (props: P) => {
+    const [isHover, setIsHover] = useState(false)
+    return (
+      <div
+        className={isHover ? styles.tadaAnimation : ""}
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
+        <BaseComponent {...(props as P)} />
+      </div>
+    )
+  }
+  return Animate
+}
--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -1,16 +1,17 @@
 import styles from "./Pokemon.module.css"
 import { Link } from "react-router-dom"
+import { Animate } from "components/Animate"
 interface Props {
   name: string
   id: number
   weight: number
   height: number
 }
-export const Pokemon = ({ name, id, height, weight }: Props) => {
+const PokemonComponent = ({ name, id, height, weight }: Props) => {
   const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
 
   return (
-    <Link to={`pokemon/${id}`} className={styles.card}>
+    <Link to={`/pokemon/${id}`} className={styles.card}>
       <p>{capitalizedString(name)}</p>
       <img src={imgUrl} alt="" />
 
@@ -22,7 +23,8 @@ export const Pokemon = ({ name, id, height, weight }: Props) => {
     </Link>
   )
 }
-
+// export const Pokemon = PokemonComponent
+export const Pokemon = Animate<Props>(PokemonComponent)
 const capitalizedString = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
